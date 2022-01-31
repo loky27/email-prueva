64460afc63a58280d79f48f61d35c6ba0d5a4b72
jonatha <hef6666@gmail.com>
2022-01-31 17:53:15 -0600

    create mentor

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import mat from './materia.mjs'
-import mentor from './mentore.mjs'
+
 import express, { application } from 'express'
 const app = express()
 const port = process.env.PORT || 3000
@@ -14,9 +14,6 @@ app.get('/',(req,res)=>{
 )
 app.get('/mate', (req, res) => {
   res.send(JSON.stringify(mat()))
-})
-apt.get('/mentor',(req,res)=>{
-  res.send(JSON.stringify(mentor()))
 })
 
 app.post('/users',(req,res)=>
